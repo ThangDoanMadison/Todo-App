01707cd04c0bd04ee491175ccc0e48e6369c9c3b
ThangDoanMadison <thang.doan@madison-technology.com>
2022-03-21 14:22:02 +0700

    Done Task Modal and Navigation

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+export default createStore({
+  state: {
+    invoiceModal: null,
+  },
+  mutations: {
+    TOGGLE_INVOICE(state) {
+      state.invoiceModal = !state.invoiceModal;
+    },
+  },
+  actions: {},
+  modules: {},
+});
